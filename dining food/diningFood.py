1d118f0d4e3cb39ba8a06c091766bee330fb4c43
nova14 <saeedghanioour1380@gamil.com>
2022-05-22 23:02:39 +0430

    change the code to a class for pall

--- dining food/diningFood.py
+++ dining food/diningFood.py
@@ -1,30 +1,42 @@
 import json
 import random
+class palling:
+    def __init__(self) -> None:
+        file = open("dining.txt", "r")
+        content = file.readline()
+        self.diningfood = json.loads(content)
+        content = file.readline()
+        self.turn = json.loads(content)
+        file.close()
+    
+    def SelectPerson(self):
+        self.dining = []
+        for key, value in self.diningfood.items():
+            if bool(value) == False:
+             self.dining.append(key)
+             
+        if len(self.dining) == 0:
+            self.dining = [key for key in self.diningfood]
+            for key in self.diningfood:
+                self.diningfood[key] = False
+        self.selected = self.dining[random.randint(0, len(self.dining) - 1)]
+        
+        self.diningfood[self.selected] = True
+        
+        file = open("dining.txt", "w")
+        file.write(json.dumps(self.diningfood))
+        file.close()
+        
+    def ReplaceTurn(self, name):
+        for key in self.diningfood:
+            if key == name:
+                self.diningfood[key] += 1        
+    
+    def ShowSelected(self):
+        print(self.selected + " is selected")
+    
+    
 
-orderFood = {}
-dining = []
-
-file = open("dining.txt", "r")
-content = file.read()
-diningfood = json.loads(content)
-file.close()
-
-for key, value in diningfood.items():
-    if bool(value) == False:
-        dining.append(key)
-
-if len(dining) == 0:
-    dining = [key for key in diningfood]
-    for key in diningfood:
-        diningfood[key] = False
-
-
-temp = dining[int(random.randint(0, len(dining)) - 1)]
-
-diningfood[temp] = True
-
-print("turn of " + temp)
-
-file = open("dining.txt", "w")
-file.write(json.dumps(diningfood))
-file.close()
+test = palling()
+test.SelectPerson()
+test.ShowSelected()
